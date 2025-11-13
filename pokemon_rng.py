92fc1ed2d4df0e41b65c22a0a0c7783447415633
Chamoisie <viardantoine2009@gmail.com>
2025-11-13 19:29:49 +0100

    pokemon in pokedex are now listed by numbers and can be deleted one by one

--- pokemon_rng.py
+++ pokemon_rng.py
@@ -2,6 +2,9 @@ import os
 import random
 from PIL import Image
 import sys
+import time
+
+
 pokedex = []
 
 SPRITE_FOLDER = "sprites"
@@ -31,16 +34,18 @@ def afficher_sprite(path):
     largeur, hauteur = image.size
     image = image.resize((largeur * facteur, hauteur * facteur), Image.NEAREST)
     image.show()
-    image.close()
+    time.sleep(3)
+possible_choices = 4
 
 while True:
     print("\nQue voulez-vous faire ?")
     print("1: RNG !")
     print("2: Open pokedex")
-    print("3: Leave(will delete your pokedex)")
-    choix = input("What do you want to do? : ")
+    print("3: Delete one pokemon (by the number)")
+    print("4: Leave(will delete your pokedex)")
+    choix = int(input("What do you want to do? : "))
 
-    if choix == "1":
+    if choix == 1:
         chemin, nom_fichier, is_shiny = rng_sprite()
         pokedex.append(nom_fichier)
         if is_shiny:
@@ -49,21 +54,18 @@ while True:
             print(f"You pulled {nom_fichier}")
         afficher_sprite(chemin)
     
-    elif choix == "2":
+    elif choix == 2:
         if pokedex:
             print("Here are your pokemons:" )
-            for i in pokedex:
-                print("-",i)
-    elif choix == "3":
+        for j in range(len(pokedex)):
+            print("-", j + 1, pokedex[j])
+
+        print(f"you have {len(pokedex)} pokemons !")
+    elif choix == 3:
+        i=int(input("Which pokemon do you want to delete?"))
+        del pokedex [i]
+    elif choix == 4:
         print("See you later")
         quit()
-
-
-            
-
-
-
-
-
-
-
+    elif choix > possible_choices:
+        print("This choice is not avaible")
